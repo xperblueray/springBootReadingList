176b342de1225d55a90b2676c87904a0ebb7e21b
xperblueray <xperblueray@outlook.com>
2019-03-01 18:27:39 +0800

    init commit

---
+++ src/main/java/com/xperblueray/codemonster/CodemonsterApplication.java
@@ -0,0 +1,13 @@
+package com.xperblueray.codemonster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemonsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodemonsterApplication.class, args);
+    }
+
+}
